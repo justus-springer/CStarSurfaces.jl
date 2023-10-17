f500fe00d91b27a1132772453dfa325905930faf
Justus Springer <justusspringer@gmx.de>
2023-10-17 11:55:53 +0200

    add docstrings for src/SurfaceWithTorusAction

--- src/SurfaceWithTorusAction/SurfaceWithTorusActionDivisor.jl
+++ src/SurfaceWithTorusAction/SurfaceWithTorusActionDivisor.jl
@@ -1,10 +1,16 @@
 
+@doc raw"""
+    SurfaceWithTorusActionDivisor = Union{CStarSurfaceDivisor, ToricSurfaceDivisor}
+
+A Weil divisor on a surface with non-trivial torus action.
+
+"""
 const SurfaceWithTorusActionDivisor = Union{CStarSurfaceDivisor, ToricSurfaceDivisor}
 
 #################################################
 # Intersection numbers
 #################################################
-#
+
 function Base.:*(d1 :: SurfaceWithTorusActionDivisor, d2 :: SurfaceWithTorusActionDivisor)
     @req d1.variety === d2.variety "The divisors must be defined on the same variety"
     c1, c2 = coefficients(d1), coefficients(d2)
